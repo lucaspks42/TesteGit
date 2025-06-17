5c18cca2c51fd28788434734017f445527dc6d28
lucaspks42 <146275321+lucaspks42@users.noreply.github.com>
2025-06-17 15:50:46 -0300

    Update TesteJava.java

--- TesteJava.java
+++ TesteJava.java
@@ -2,5 +2,6 @@ public class  ClasseTeste{
 
         public static void main(String[] args) {
                 System.out.println("Olá, mundo!");
+                System.out.println("Teste de pull");
     }
 }
