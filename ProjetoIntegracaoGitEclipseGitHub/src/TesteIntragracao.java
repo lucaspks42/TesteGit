6f44bb50619d5746ff5443442af8cd1a3323b954
lucaspks42 <146275321+lucaspks42@users.noreply.github.com>
2025-07-24 16:23:17 -0300

    Update TesteIntragracao.java

--- ProjetoIntegracaoGitEclipseGitHub/src/TesteIntragracao.java
+++ ProjetoIntegracaoGitEclipseGitHub/src/TesteIntragracao.java
@@ -3,5 +3,6 @@ public class TesteIntragracao {
 	public static void main(String[] args) {
 		System.out.println("Primeiro teste");
 		System.out.println("teste");
+		System.out.println("Commit vindo do git");
 	}
 }
