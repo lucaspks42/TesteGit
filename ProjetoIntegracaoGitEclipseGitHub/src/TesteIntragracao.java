b4176755bdc45ec3f86b2a6912e488c9a8deed91
lucaspks42 <lucaspmoraes603@gmail.com>
2025-07-24 16:17:42 -0300

    Segundo commit

--- ProjetoIntegracaoGitEclipseGitHub/src/TesteIntragracao.java
+++ ProjetoIntegracaoGitEclipseGitHub/src/TesteIntragracao.java
@@ -2,5 +2,7 @@
 public class TesteIntragracao {
 	public static void main(String[] args) {
 		System.out.println("Primeiro teste");
+		System.out.println("Segundo teste");
+		System.out.println("Terceiro Teste");
 	}
 }
